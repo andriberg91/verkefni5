455434d0cf6ca7ecbd75e9b7aaffb84f9626e414
andriberg19 <andri19@ru.is>
2019-09-05 12:02:29 +0000

    Algorithm written for sequence

---
+++ sequence.py
@@ -0,0 +1,4 @@
+# n verður fjölda talna sem fara í röðina
+# Regla á röðinni verður að eftir töluna 3 verður næsta tala summa fyrri þriggja talna sem koma undan í röðinni
+# Búið verður til 3 temp sem halda tölunum og þær færast síðan um eitt sæti þegar ný tala kemur inn
+
